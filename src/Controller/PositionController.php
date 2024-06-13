ddd1d087bd97e6d0ef7356e54b6c4e624ca6173c
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-06-13 15:31:41 +0300

    Update the Positions page. All columns are visible on the page, without scrolling.

--- src/Controller/PositionController.php
+++ src/Controller/PositionController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Position;
+use App\Entity\PositionState;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PositionController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
     #[Route('position/show', name: 'app_closed_position_show_all')]
     public function showAllPositions(): Response
     {
-        $positions = [
-            'GOLD' => ['open_at' => 2330.00, 'order' => 'buy', 'closed_at' => 2450.25, 'profit' => 380.30],
-            'SP500' => ['open_at' => 5300.00, 'order' => 'buy', 'closed_at' => 5428.01, 'profit' => 483.55],
-            'NAS100' => ['open_at' => 10300.00, 'order' => 'buy', 'closed_at' => 10428.01, 'profit' => 183.55]
-        ];
-
+        $positions = $this->entityManager->getRepository(PositionState::class)->findAll();
+//        dd($positions);
         return $this->render('position/closed_positions.html.twig', [
             'positions' => $positions,
         ]);
